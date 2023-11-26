42b324ebf4fdbf6ea482f835401d2edc383f2ba4
gouzidev <gouzi.dev@gmail.com>
2023-11-26 20:30:07 +0100

    bruh

--- get_next_line.c
+++ get_next_line.c
@@ -123,28 +123,39 @@ char *get_line(t_list *list)
 	}
 	return (line);
 }
+void freee(t_list **list)
+{
+	t_list *next;
 
+	next = *list;
+	while (list && *list && (*list)->next)
+	{
+		next = (*list)->next;
+		free((*list)->str);
+		free(*list);
+		*list = next;
+	}
+}
 t_list* cleaner(t_list **list, t_list *new_node, char **rest)
 {
 	int i;
 	int j;
-	t_list *fixed_node;
-	t_list *head;
+	t_list *temp;
 	t_list *next;
+	char *buff;
 
-	fixed_node = malloc(sizeof(t_list));
-	fixed_node->str = malloc(ft_strlen(new_node->str, '_'));
-	head = *list;
-	fixed_node->next = NULL;
-	while (*list)
+	buff = malloc(ft_strlen(new_node->str, '_'));
+	temp = *list;
+	while (temp)
 	{
-		if (*list == new_node)
+		if (temp == new_node)
 		{
 			i = -1;
+			printf("new_node->str    %s\n", new_node->str);
 			while (++i >= 0 && new_node->str[i] && new_node->str[i] != '_')
-			{
-				fixed_node->str[i] = new_node->str[i];
-			}
+				buff[i] = new_node->str[i];
+			buff[i] = '\0';
+			printf("buff             %s\n", buff);
 			if (new_node->str[i] == '_')
 				i++;
 			(*rest) = malloc(ft_strlen(&new_node->str[i], '_'));
@@ -152,17 +163,12 @@ t_list* cleaner(t_list **list, t_list *new_node, char **rest)
 			while (new_node->str[i] && new_node->str[i] != '_')
 				(*rest)[j++] = new_node->str[i++];
 			(*rest)[j] = '\0';
-			while (head)
-			{
-				next = head->next;
-				free(head);
-				head = next;
-			}
-			printf("fixed -> %s\n", fixed_node->str);
-			*list = fixed_node;
-			return (fixed_node);
+			printf("rest             %s\n", *rest);
+			freee(list);
+			new_node->str = buff;
+			printf("new ->           %s\n", new_node->str);
 		}
-		*list = (*list)->next;
+		temp = (temp)->next;
 	}
 }
 
@@ -170,13 +176,20 @@ char *get_next_line(int fd)
 {
 	int len;
 	char *buff = malloc (SIZE + 1);
-	static t_list *list;
+	 t_list *list;
 	static char *rest;
 	char *line;
 	t_list *new_node;
 	list = NULL;
 	int bytes;
 	len = 0;
+	printf("REST HAD %s\n", rest);
+	if (rest)
+	{
+		new_node = ft_lstnew(rest);
+		ft_lstadd_back(&list, new_node, &len);
+	}
+
 	while ((bytes = read(fd, buff, SIZE)) > 0)
 	{
 		new_node = ft_lstnew(buff);
@@ -184,18 +197,18 @@ char *get_next_line(int fd)
 		if (found_nl(buff))
 		{
 			line = get_line(list);
-			new_node = cleaner(&list, new_node, &rest);
+			cleaner(&list, new_node, &rest);
 			printf("line ->  %s\n", line);
-			printf("rest ->  %s\n", rest);
 		}
 	}
-		ft_lstprint(list);
 }
 #include <string.h>
 int main()
 {
 
 	int fd = open("FILE", O_RDONLY);
+	get_next_line(fd);
+	get_next_line(fd);
 	get_next_line(fd);
 }
 #endif
